a2b4e5f2d06ec6513e418bd4ca0ec881a7ffe2be
teimuraz <teimuraz.kantaria@gmail.com>
2018-12-24 22:32:08 +0400

    Initial commit.

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "benchmark-akka"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
+
+libraryDependencies += "com.typesafe.akka" %% "akka-http"   % "10.1.6"
+libraryDependencies += "com.typesafe.akka" %% "akka-stream" % "2.5.19" // or whatever the latest version is
---
+++ src/main/scala/com/scaltrack/benchmark/WebServer.scala
@@ -0,0 +1,56 @@
+package com.scaltrack.benchmark
+
+import java.util.Random
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.io.StdIn
+
+object WebServer {
+
+  val rand = new Random
+
+  def makeRequests(implicit sys: ActorSystem, mat: ActorMaterializer, ec: ExecutionContext): Future[String] = {
+
+    val futures = (1 to 10).map( _ =>
+      Http().singleRequest(HttpRequest(uri = s"http://3.17.161.135:9001/hello?p=${rand.nextInt(99999) + 1}")).map(response => response.toString())
+    )
+
+    Future.sequence(futures).map( _ => "Done")
+
+  }
+
+  def main(args: Array[String]) {
+
+    implicit val system = ActorSystem("my-system")
+    implicit val materializer = ActorMaterializer()
+    // needed for the future flatMap/onComplete in the end
+    implicit val executionContext = system.dispatcher
+
+
+    val route =
+      path("hello") {
+        get {
+          val r = makeRequests
+
+          onSuccess(r) { resp =>
+            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "Done"))
+          }
+        }
+      }
+
+    val bindingFuture = Http().bindAndHandle(route, "localhost", 9001)
+
+    println(s"Server online at http://localhost:9001/\nPress RETURN to stop...")
+    StdIn.readLine() // let it run until user presses return
+    bindingFuture
+      .flatMap(_.unbind()) // trigger unbinding from the port
+      .onComplete(_ => system.terminate()) // and shutdown when done
+  }
+}
+
